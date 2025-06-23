1dff8cc17ca13908f1a4045d962248913b0f6729
janus9266 <iamjanus66@gmail.com>
2025-06-23 04:08:13 -0400

    send vi sms progressing

--- src/app/api/sms/vi/sending/route.ts
+++ src/app/api/sms/vi/sending/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-// import {Apidaze} from '@apidaze/node';
+import {Apidaze} from '@apidaze/node';
 
 export async function POST(request: NextRequest) {
   try {
@@ -7,23 +7,29 @@ export async function POST(request: NextRequest) {
     // const to = body.to;
     // const messageBody = body.messageBody;
 
-  //   const to = '16027370077';
-  //   const messageBody = 'Hello, this is a test message from the VI SMS API';
+    const to = '447427844303';
+    const messageBody = 'Hello, this is a test message from the VI SMS API';
 
-  //   const ApidazeClient = new Apidaze({
-  //     apiKey: '29mfb0z0',
-  //     apiSecret: 'cfc2e6061d9ab625e388ba6e431fc57b',
-  //   });
+    console.log(to,messageBody);
 
-  //   const response = await ApidazeClient.messages.send('13102887998', to, messageBody);
+    const ApidazeClient = new Apidaze(
+      'rts01d7g',
+      '8f02cb9cc4c8670ab5aebb364c094b2e',
+    );
 
-  //   console.log(response);
+    console.log(ApidazeClient);
 
-  // } catch (error) {
-  //   console.error('Error sending SMS:', error);
-  //   return new Response(
-  //     JSON.stringify({ error: (error as Error).message }),
-  //     { status: 500 }
-  //   );
-  // }
+    const response = await ApidazeClient.messages.send('18556949555', to, messageBody);
+
+    console.log(response);
+
+    return new Response(JSON.stringify({ success: true, data: response }), { status: 200 });
+
+  } catch (error) {
+    console.error('Error sending SMS:', error);
+    return new Response(
+      JSON.stringify({ error: (error as Error).message }),
+      { status: 500 }
+    );
+  }
 }
